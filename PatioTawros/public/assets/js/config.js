d785271765701a12c52ef9bb01860436defab41d
sistematawros-dev <sistematawros@gmail.com>
2025-10-15 22:06:53 -0300

    Update API_BASE URL in config.js

--- PatioTawros/public/assets/js/config.js
+++ PatioTawros/public/assets/js/config.js
@@ -1,2 +1,2 @@
 // Configuração do cliente
-const API_BASE = localStorage.getItem('API_BASE') || 'http://localhost:3333';
+const API_BASE = localStorage.getItem('API_BASE') || 'http://209.38.154.140:3333';
